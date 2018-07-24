00a6d04c82310507b6f30284989e75cb2ffa919c
brijeshlakkad <brijeshlakkad@github.com>
2018-07-24 09:20:57 +0530

    added profile.php to show profile of user

--- profile.php
+++ profile.php
@@ -1,9 +1,14 @@
-<?php include("header.php"); ?>
+<?php include_once("header.php"); ?>
+<?php check_session(); ?>
 <div class="container-fluid my_well">
-	<div class="row">
-		<h2><b>Hello</b></h2>
-		<br>
-		This is brijesh.
+	<div class="row" align="center">
+		<h2 class="c_header">Jon Snow is here.</h2>
+		<div class="row" style="margin-top: 20px;">
+			<div class="row" style="margin: 50px;">
+				Want Help?
+			</div>
+			<button class="btn btn-primary" >Ask questions to me >></button>
+		</div>
 	</div>
 </div>
 <div class="please_wait_modal"></div>
