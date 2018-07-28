844322c4d015466ac020ea1f4c4ec4aa52e52911
brijeshlakkad <brijeshlakkad@github.com>
2018-07-28 12:26:21 +0530

    added footer in index.php

--- index.php
+++ index.php
@@ -1,2 +1,85 @@
 <?php include("header.php"); ?>
+<link rel="stylesheet" href="css/footer.css">
+<div class="container-fluid blurclass"  style="padding:0px;height:700px;width:100%;">
+	
+</div>
+<footer class="footer">
+    <nav class="footer__nav shuffle shuffled">
+        <section class="footer__col footer__col--intro">
+            <a class="footer__logo" href="/" data-footer-link="1">
+             <img src="images/jonsnow.png" alt="Jon Snow" width="150px"/>
+            </a>
+
+            <p class="footer__slogan">
+                AI Chatbots are the future.
+                <strong>Be Bold.</strong>
+            </p>
+        </section>
+
+        <section class="footer__col">
+            <div class="footer__col-wrap">
+              <h4 class="footer__col-title">Explore</h4>
+
+              <div class="footer__nav-list">
+                <a href="index.php" class="footer__link" data-footer-link="1">Home</a>
+                <a href="about.php" class="footer__link" data-footer-link="2">About</a>
+                <a href="contact.php" class="footer__link" data-footer-link="3">Contact</a>
+              </div>
+            </div>
+        </section>
+
+        <section class="footer__col">
+            <div class="footer__col-wrap">
+                <h4 class="footer__col-title">Visit</h4>
+
+                <a class="footer__address" href="https://www.bvmengineering.ac.in" target="_blank">
+                    <span class="footer__address-row">Jon Snow Building</span>
+                    <span class="footer__address-row">Birla Vishvakarma Mahavidyalaya Engineering College</span>
+                    <span class="footer__address-row">Anand, Gujarat, India</span>
+                </a>
+    
+                <h4 class="footer__col-title footer__hide--small">New Business</h4>
+                <a href="mailto:tempacc2179@gmail.com" class="footer__link footer__hide--small">tempacc2179@gmail.com</a>
+                <a href="tel:7046167267" class="footer__link footer__hide--small">+91 7046 167 267</a>
+            </div>
+        </section>
+
+        <section class="footer__col footer__col--mobile footer__hide--large background--active">
+            <div class="footer__col-wrap">
+                <h4 class="footer__col-title">New Business</h4>
+                <a href="mailto:engage@weareenvoy.com" class="footer__link">Email us</a>
+                <a href="tel:9497354092" class="footer__link">949 735 4092</a>
+            </div>
+        </section>
+
+        <section class="footer__col">
+            <div class="footer__col-wrap">
+                <h4 class="footer__col-title">Follow</h4>
+                <a href="https://www.instagram.com/jon_snow_chatbot/" target="_blank" class="footer__link no-barba">Instagram</a>
+                <a href="https://twitter.com/jon_snow_chatbot" target="_blank" class="footer__link no-barba">Twitter</a>
+                <a href="https://www.linkedin.com/company/jon_snow_chatbot" target="_blank" class="footer__link no-barba">LinkedIn</a>
+            </div>
+        </section>
+
+        <section class="footer__col">
+            <div class="footer__col-wrap">
+                <h4 class="footer__col-title">Legal</h4>
+                <a href="/terms" class="footer__link">Terms</a>
+                <a href="/privacy" class="footer__link">Privacy</a>
+            </div>
+        </section>
+
+        <section class="footer__col footer__col--next">
+            <a href="/about" class="footer__btn" data-footer-link="2">
+                <span>Next: About</span>
+
+                <div class="footer__btn-line">
+                    <div class="footer__btn-line-inner"></div>
+                </div>
+            </a>
+        </section>
+
+        <small class="footer__legal">© 2018 Jon Assistant. All Rights Reserved.</small>
+    </nav>
+  </footer>
 <?php include("footer.php"); ?>
