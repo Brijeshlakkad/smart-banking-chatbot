6e6d44ba6e19cce3f066ae5cd5a94634515b2381
brijeshlakkad <brijeshlakkad@github.com>
2018-07-31 21:52:34 +0530

    Finds file using username in specific directory

---
+++ find_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import config
+import os
+import fnmatch
+def find_customer_files(username):
+	root="customer_files"
+	listOfFiles = os.listdir(os.path.join(root,username))
+	patterns = ["pdf","jpeg","jpg"]  
+	for entry in listOfFiles:  
+		for p in patterns:
+			if entry.endswith(p):
+				print(entry)
+def find_customer_files(username):
+	root="customer_images"
+	listOfFiles = os.listdir(os.path.join(root,username))
+	patterns = ["pngs","jpeg","jpg"]  
+	for entry in listOfFiles:  
+		for p in patterns:
+			if entry.endswith(p):
+				print(entry)
