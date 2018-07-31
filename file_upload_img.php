e8458ae896136fbcccf8f9c215a2cda96eeb52ed
brijeshlakkad <brijeshlakkad@github.com>
2018-07-31 13:49:01 +0530

    added part 3 of signup form (upload pic)

---
+++ file_upload_img.php
@@ -0,0 +1,61 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+if(!isset($_COOKIE["Username"])) {
+    header("Location:unreachable.php");
+} else {
+$var_temp=$_COOKIE['Username'];
+$var=trim($var_temp);
+$allowedExts = array("jpeg", "jpg","png");
+$temp = explode(".", $_FILES["image"]["name"]);
+$extension = end($temp);
+if ((($_FILES["image"]["type"] == "image/jpg") || ($_FILES["image"]["type"] == "image/jpeg") || ($_FILES["image"]["type"] == "image/png"))
+&& in_array($extension, $allowedExts))
+  {
+  if ($_FILES["image"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["image"]["error"] . "<br>";
+    }
+  else
+    {
+   if (!file_exists('customer_images/'.$var)) {
+    mkdir('customer_images/'.$var, 0777, true);
+   }
+		if($_FILES["image"]["type"] == "image/jpg")
+		  {
+			  $ext="jpg";
+		  }
+		  else if($_FILES["image"]["type"] == "image/jpeg")
+		  {
+			  $ext="jpeg";
+		  }
+		  else if($_FILES["image"]["type"] == "image/png")
+		  {
+			  $ext="png";
+		  }
+		  else
+		  {
+			  header("Location:unreachable.php");
+		  }
+		$cfilename=$var.".".$ext;
+		$path_to_file="customer_images/".$var."/";
+		if(file_exists($path_to_file))
+		{
+			if(file_exists($path_to_file."".$var.".jpeg"))
+				unlink($path_to_file."".$var.".jpeg");
+			if(file_exists($path_to_file."".$var.".jpg"))
+				unlink($path_to_file."".$var.".jpg");
+			if(file_exists($path_to_file."".$var.".png"))
+				unlink($path_to_file."".$var.".png");
+		}
+		move_uploaded_file($_FILES["image"]["tmp_name"],$path_to_file."".$cfilename);
+     	setcookie('Username', null, -1, '/');
+		header("Location:profile_set.php");
+    }
+  }
+else
+  {
+  die("Invalid file or Bigger size");
+  }
+}
+?>
---
+++ signup_part3.php
@@ -0,0 +1,100 @@
+<?php include("header.php"); 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+if(!isset($_COOKIE["Username"])) {
+    header("Location:unreachable.php");
+} else {
+$username=trim($_COOKIE["Username"]);
+	?>
+<center>
+<div class="container well login_block" align="center">
+	<div class="row center-block ">
+		<div><caption><a href="#"><img src="images/jonsnow.png" class="img-responsive" style="margin-top:10px;width:250px;height:60px;float:center;filter:drop-shadow(0px 0px 3px #ffffff);"/></a></caption></div>
+	</div>
+	<div class="row">
+<form name="i_form" method="post" enctype="multipart/form-data" action="file_upload_img.php">
+<table class="myTable">
+	<tr>
+	<div class="form-group">
+		<td><label for="a_image" class="control-label">Profile picture</label></td>
+		<td><input class="form-control" type="file" id="file" name="image" /></td>
+		<td><p id="a_img"></p></td>
+	</tr>
+	<tr>
+	<div class="form-group">
+		<td></td><td><button class="btn btn-default form-control" type="button" id="uploadimage" onclick="check()">Upload Pic</button></td>
+	</div>
+	</tr>
+	<tr>
+		<td></td>
+		<td><a class="btn btn-default form-control" type="button" href="candidate_default_pic.php">Skip</a>
+			
+				</td>
+	</tr>
+</table>
+</form>
+	
+<div id="image_preview">
+	<img id="previewing" src="Images/noimage.png" alt="no image" />
+</div>
+</div>
+</div>
+</div>
+</center>
+
+<script>
+	var myApp = angular.module("myapp", []);
+	myApp.controller("BrijController", function($scope,$http) {
+	});
+	
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip(); 
+	$("#spinner").hide();
+});
+</script>
+<script>
+	function check()
+	{
+		var a = document.getElementById("a_img").innerHTML;
+		var b = $("#file").val();
+		if(b=="")
+			{
+				$("#a_img").html("<p style='color:red;'>Please select a valid Image File <br> Only jpeg, jpg and png images type allowed </p>");
+			}
+		else if(a=="" && b!="")
+			document.i_form.submit();
+	}
+$(document).ready(function (e) {
+$(function() {
+$("#file").change(function() {
+$("#a_img").empty();
+var file = this.files[0];
+var match= ["image/jpeg","image/png","image/jpg"];
+if(!((file.type==match[0]) || (file.type==match[1]) || (file.type==match[2])))
+{
+$('#previewing').attr('src','../images/noimage.png');
+$("#a_img").html("<p style='color:red;'>Please select a valid Image File <br> Only jpeg, jpg and png images type allowed </p>");
+return false;
+}
+else
+{
+var reader = new FileReader();
+reader.onload = imageIsLoaded;
+reader.readAsDataURL(this.files[0]);
+}
+});
+});
+function imageIsLoaded(e) {
+$("#file").css("color","green");
+$('#image_preview').css("display", "block");
+$('#previewing').attr('src', e.target.result);
+$('#previewing').attr('width', '200px');
+$('#previewing').attr('height', '180px');
+};
+});
+</script>
+	<?php
+}
+?>
+
+<?php include("footer.php"); ?>
