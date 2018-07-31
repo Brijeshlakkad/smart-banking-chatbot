f2428c739650df2f1d60ffc3ab58803f82b472e3
brijeshlakkad <brijeshlakkad@github.com>
2018-07-31 13:49:36 +0530

    when no results are made

---
+++ no_found.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import sys
+import os
+def no_found(str_show):
+	print("""<div class="row" align="center" style="margin-top: 80px;">
+	<div id="no_found"><img src="images/not-found.png" width="100px" alt="no found" /></div>
+	<br/>
+	<div style="color:gray;"><h4>%s</h4></div>
+	</div>"""%str_show)
