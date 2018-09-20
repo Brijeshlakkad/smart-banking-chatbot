a89b689ba191658830425201de37ee949d9dab58
brijeshlakkad <brijeshlakkad@github.com>
2018-09-20 13:32:23 +0530

    improved model

--- train/Bank_bot.py
+++ train/Bank_bot.py
@@ -23,12 +23,7 @@ class BankAPI(object):
 def train_dialogue(domain_file="bank_domain.yml",model_path="models/dialogue/",training_data_file="data/stories.md"):
     agent = Agent(domain_file,policies=[MemoizationPolicy(max_history=3),KerasPolicy()])
     training_data = agent.load_data(training_data_file)
-    agent.train(
-            training_data,
-            epochs=400,
-            batch_size=100,
-            validation_split=0.2
-    )
+    agent.train(training_data,batch_size=200,epochs=150,validation_split=0.2)
     agent.persist(model_path)
     return agent
 
