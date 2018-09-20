155ed44b92fc46553e8ab76a17b1b18b65523f91
brijeshlakkad <brijeshlakkad@github.com>
2018-09-20 21:32:16 +0530

    All errors and bot behavior is improved

--- train/actions.py
+++ train/actions.py
@@ -6,15 +6,14 @@ from __future__ import print_function
 from __future__ import unicode_literals
 import warnings
 warnings.filterwarnings('always')
-from rasa_core.actions.action import Action,ActionListen
+from rasa_core.actions.action import *
 from rasa_core.actions.forms import *
 from rasa_core.events import *
 from jon_working_with_db import *
 class GetAccess(FormAction):
     RANDOMIZE = False
     @staticmethod
-    def required_fields
-    ():
+    def required_fields():
         return [
         FreeTextFormField("email"),
         FreeTextFormField("password")
@@ -32,23 +31,23 @@ class GetAccess(FormAction):
             return [ActionReverted(),AllSlotsReset()]
         dispatcher.utter_template("utter_access",tracker)
         return [SlotSet("access", results)]
-class GetCardService(FormAction):
+class ActivateCardService(FormAction):
     RANDOMIZE = False
     @staticmethod
     def required_fields():
         return [
         BooleanFormField("card_permission","affirm","deny"),
-        FreeTextFormField("passcode")
+        FreeTextFormField("passcode_1")
         ]
     def name(self):
-        return 'action_card_service'
+        return 'action_activate_card'
     def submit(self, dispatcher, tracker, domain):
         user,password=tracker.get_slot("email"),tracker.get_slot("password")
         access=tracker.get_slot("access")
         if user==None or password==None or access!=1:
             dispatcher.utter_message("Please log in our service, to use Jon service!")
             return [ActionReverted(),AllSlotsReset()]
-        user,password,passcode=tracker.get_slot("email"),tracker.get_slot("password"),tracker.get_slot("passcode")
+        user,password,passcode=tracker.get_slot("email"),tracker.get_slot("password"),tracker.get_slot("passcode_1")
         card_permission=tracker.get_slot("card_permission")
         question_yes_no=tracker.get_slot("question_yes_no")
         using_what=tracker.get_slot("using_what")
@@ -58,14 +57,66 @@ class GetCardService(FormAction):
         ent="card"
         template="utter_fallback"
         if results==1:
-            tracker.update(UserUtteranceReverted())
-            tracker.update(UserUtteranceReverted())
-            entities=tracker.latest_message.entities
-            intent=tracker.latest_message.intent['name']
+            t1=tracker.copy()
+            t1.update(UserUtteranceReverted())
+            t1.update(UserUtteranceReverted())
+            entities=t1.latest_message.entities
+            intent=t1.latest_message.intent['name']
             if intent=="Banking_Activate_Card":
                 results=get_active_card(user,password,passcode)
                 template="utter_activated_card"
-            elif intent=="Banking_Cancel_Card":
+            len1=len(entities)
+            for i in range(len1):
+                if entities[i]["entity"]=="card_type":
+                    ent=entities[i]['value']
+                elif entities[i]["entity"]=="using_what":
+                    using_what=entities[i]['value']
+                elif entities[i]["entity"]=="question_yes_no":
+                    question_yes_no=entities[i]['value']
+            if question_yes_no!=None:
+                template="utter_give_pos_ans"
+                using=""
+                if using_what!=None:
+                    using=" ".join(["using",using_what])
+                dispatcher.utter_template(template,tracker,using=using)
+            else:
+                dispatcher.utter_template(template,tracker,card_type=ent.capitalize())
+        else:
+            dispatcher.utter_message("Please enter valid information")
+            return [ActionReverted()]
+        return [SlotSet("card_permission",None),SlotSet("passcode_1",None)]
+class CancelCardService(FormAction):
+    RANDOMIZE = False
+    @staticmethod
+    def required_fields():
+        return [
+        BooleanFormField("card_permission","affirm","deny"),
+        FreeTextFormField("passcode_2")
+        ]
+    def name(self):
+        return 'action_cancel_card'
+    def submit(self, dispatcher, tracker, domain):
+        user,password=tracker.get_slot("email"),tracker.get_slot("password")
+        access=tracker.get_slot("access")
+        if user==None or password==None or access!=1:
+            dispatcher.utter_message("Please log in our service, to use Jon service!")
+            return [ActionReverted(),AllSlotsReset()]
+        user,password,passcode=tracker.get_slot("email"),tracker.get_slot("password"),tracker.get_slot("passcode_2")
+        card_permission=tracker.get_slot("card_permission")
+        question_yes_no=tracker.get_slot("question_yes_no")
+        using_what=tracker.get_slot("using_what")
+        if passcode==None or card_permission!=True:
+            return [ActionReverted()]
+        results = check_passcode(user,password,passcode)
+        ent="card"
+        template="utter_fallback"
+        if results==1:
+            t1=tracker.copy()
+            t1.update(UserUtteranceReverted())
+            t1.update(UserUtteranceReverted())
+            entities=t1.latest_message.entities
+            intent=t1.latest_message.intent['name']
+            if intent=="Banking_Cancel_Card":
                 results=get_deactive_card(user,password,passcode)
                 template="utter_deactivated_card"
             len1=len(entities)
@@ -87,7 +138,7 @@ class GetCardService(FormAction):
         else:
             dispatcher.utter_message("Please enter valid information")
             return [ActionReverted()]
-        return [SlotSet("passcode",None),SlotSet("card_permission",None)]
+        return [SlotSet("card_permission",None),SlotSet("passcode_2",None)]
 class GetFeeInquiry(Action):
     def name(self):
         return 'action_fee_inquiry'
@@ -109,7 +160,7 @@ class CardReplaceService(FormAction):
         return [
         FreeTextFormField("card_replace_with"),
         BooleanFormField("card_perm","affirm","deny"),
-        FreeTextFormField("passcode")
+        FreeTextFormField("passcode_rep")
         ]
     def name(self):
         return 'action_card_replace'
@@ -119,7 +170,7 @@ class CardReplaceService(FormAction):
         if user==None or password==None or access!=1:
             dispatcher.utter_message("Please log in our service, to use Jon service!")
             return [ActionReverted(),AllSlotsReset()]
-        user,password,passcode=tracker.get_slot("email"),tracker.get_slot("password"),tracker.get_slot("passcode")
+        user,password,passcode=tracker.get_slot("email"),tracker.get_slot("password"),tracker.get_slot("passcode_rep")
         card_perm=tracker.get_slot("card_perm")
         card_replace_with=tracker.get_slot("card_replace_with")
         if passcode==None or card_replace_with==None or card_perm!=True:
@@ -132,4 +183,4 @@ class CardReplaceService(FormAction):
         if results!=1:
             dispatcher.utter_message("Please try again! or login again!")
         dispatcher.utter_template("utter_replace_card_reply",tracker)
-        return [SlotSet("passcode",None),SlotSet("card_perm",None)]
+        return [SlotSet("passcode_rep",None),SlotSet("card_perm",None)]
--- train/training_online.py
+++ train/training_online.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 def run_bank_online(input_channel, interpreter,domain_file="bank_domain.yml",training_data_file='data/stories.md'):
     model_path="models/dialogue/"
     agent = Agent(domain_file,policies=[MemoizationPolicy(max_history=10), KerasPolicy()],interpreter=interpreter)
-    agent.train_online(training_data_file,input_channel=input_channel,batch_size=200,epochs=150)
+    agent.train_online(training_data_file,input_channel=input_channel,batch_size=200,epochs=150,max_training_samples=1000)
     agent.persist(model_path)
     return agent
 if __name__ == '__main__':
