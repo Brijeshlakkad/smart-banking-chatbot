e1d727a549738001d51e6952ba4d1560271a9f7c
Brijeshlakkad <Brijeshlakkad@github.com>
2018-10-14 20:45:11 +0530

    Added function to send otp only giving user mail id

--- send_mail.py
+++ send_mail.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 import private_data
-from smtplib import SMTP_SSL as SMTP 
+from smtplib import SMTP_SSL as SMTP
 from email.mime.text import MIMEText
+import random
+from config import connect_to_database
 class Mail:
 	USERNAME = private_data.admin_email
 	PASSWORD = private_data.admin_password
@@ -15,7 +17,7 @@ class Mail:
 		try:
 			msg = MIMEText(content, text_subtype)
 			msg['Subject']= subject
-			msg['From']   = sender 
+			msg['From']   = sender
 			conn = SMTP(SMTPserver)
 			conn.set_debuglevel(False)
 			conn.login(self.USERNAME, self.PASSWORD)
@@ -26,3 +28,22 @@ class Mail:
 				conn.quit()
 		except Exception:
 			return "-99"
+	def send_otp_mail(self,user):
+		otp=""
+		for i in range(0,6):
+			otp+="%s"%(random.randint(0,9))
+		sub="OTP"
+		cont="Your OTP is: <b>%s</b>"%otp
+		conn,cursor=connect_to_database()
+		sql="update customers SET last_otp='%s' where email='%s'"%(otp,user)
+		try:
+			cursor.execute(sql)
+			conn.commit()
+			if self.send_mail(user,sub,cont)=="11":
+				return otp
+			return -99
+		except:
+			conn.rollback()
+			return -99
+		finally:
+			conn.close()
