55c1539d262b6ed87f7d262093770591fa04438f
Brijeshlakkad <Brijeshlakkad@github.com>
2018-10-18 21:54:58 +0530

    Dropped unusal tables.

--- minor_project.sql
+++ minor_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 15, 2018 at 10:00 AM
+-- Generation Time: Oct 18, 2018 at 04:24 PM
 -- Server version: 5.6.38
 -- PHP Version: 7.2.1
 
@@ -43,26 +43,10 @@ CREATE TABLE `accounts` (
 --
 
 INSERT INTO `accounts` (`acc_id`, `c_id`, `acc_name`, `acc_type`, `balance`, `created_time`, `passcode`, `acc_no`, `status`) VALUES
-(4, 70, 'Brijesh Rameshbhai Lakkad', 'saving', '0', '2018-08-01 20:24:57', '123456', '007046167267', 1),
-(16, 1, 'Brijesh Rameshbhai Lakkad', 'saving', '0', '2018-08-06 17:33:32', '-99', '007046167268', 1),
+(4, 70, 'Brijesh Rameshbhai Lakkad', 'saving', '0', '2018-08-01 20:24:57', '123456', '007046167269', 1),
+(16, 1, 'Brijesh Rameshbhai Lakkad', 'saving', '2411', '2018-08-06 17:33:32', '-99', '007046167268', 1),
 (19, 100, 'Sanket Patel', 'saving', '0', '2018-08-20 15:44:59', '123456', '007046167290', 1),
-(20, 101, 'Brijesh Rameshbhai Lakkad', 'saving', '0', '2018-08-20 16:15:47', '683249', '007046167255', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bank_atm`
---
-
-CREATE TABLE `bank_atm` (
-  `bid` int(10) NOT NULL,
-  `area` varchar(10) NOT NULL,
-  `city` varchar(10) NOT NULL,
-  `state` varchar(10) NOT NULL,
-  `country` varchar(10) NOT NULL,
-  `branch_atm` varchar(30) NOT NULL,
-  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+(20, 101, 'Brijesh Rameshbhai Lakkad', 'saving', '1889', '2018-08-20 16:15:47', '683249', '007046167267', 1);
 
 -- --------------------------------------------------------
 
@@ -89,8 +73,7 @@ CREATE TABLE `cards` (
 
 INSERT INTO `cards` (`card_id`, `c_id`, `acc_id`, `holder_name`, `till_month`, `till_year`, `csv`, `card_type`, `card_no`, `status`) VALUES
 (10, 70, 4, 'Brijesh Rameshbhai Lakkad', '08', '19', '494', 'Mastercard', '2685498962451745', 1),
-(38, 100, 19, 'Sanket Patel', '10', '19', '656', 'Mastercard', '5743344187443345', 1),
-(44, 101, 20, 'Brijesh Rameshbhai Lakkad', '10', '19', '970', 'Mastercard', '2428205825599963', 0);
+(38, 100, 19, 'Sanket Patel', '10', '19', '656', 'Mastercard', '5743344187443345', 1);
 
 -- --------------------------------------------------------
 
@@ -161,9 +144,9 @@ CREATE TABLE `customers` (
 
 INSERT INTO `customers` (`cid`, `username`, `fname`, `lname`, `email`, `password`, `contact`, `postal_add`, `perm_add`, `city`, `state`, `country`, `middle_name`, `pincode`, `dob`, `gender`, `time`, `created_time`, `jon`, `hasAcc`, `last_otp`) VALUES
 (1, 'brijeshlakkad22222', 'Brijesh', 'Lakkad', 'lakkadbrijesh@gmail.com', '12345b', '7046167268', 'xyz', 'xyz', 'surat', 'gujarat', 'india', 'Rameshbhai', '395006', NULL, 'Male', '2018-07-27 08:56:55', '2018-07-27 16:33:37', 0, 1, '-99'),
-(70, 'Brijesh', 'Brijesh', 'Lakkad', 'brij79lakkad@gmail.com', '123456bB', '7046167267', '205, Nanddham Apartment', 'Near Ashok Vatika Society, L.H.Road', 'Surat', 'Gujarat', 'India', 'Rameshbhai', '395008', '2018-07-03', 'Male', '2018-07-31 13:44:07', '2018-07-31 13:44:07', 1, 1, '-99'),
+(70, 'Brijesh', 'Brijesh', 'Lakkad', 'brij79lakkad@gmail.com', '123456bB', '7046167269', '205, Nanddham Apartment', 'Near Ashok Vatika Society, L.H.Road', 'Surat', 'Gujarat', 'India', 'Rameshbhai', '395008', '2018-07-03', 'Male', '2018-07-31 13:44:07', '2018-07-31 13:44:07', 1, 1, '-99'),
 (100, 'sanketpatel2', 'Sanket', 'Patel', 'patelsanket864@gmail.com', '123456bB', '7046167290', '205, Nanddham Apartment', 'Near Ashok Vatika Society, L.H.Road', 'Surat', 'Gujarat', 'India', 'J', '395006', '2018-08-08', 'Male', '2018-08-20 15:13:11', '2018-08-20 15:13:11', 1, 0, '-99'),
-(101, 'brijeshlakkad', 'Brijesh', 'Lakkad', 'brijeshlakkad22@gmail.com', '123456bb', '1010101010', 'line 2, line 3', 'Near Ashok Vatika Society, L.H.Road', 'Surat', 'Gujarat', 'India', 'R', '395006', '2018-08-08', 'Male', '2018-08-20 16:12:13', '2018-08-20 16:12:13', 1, 1, '469819');
+(101, 'brijeshlakkad22', 'Brijesh', 'Lakkad', 'brijeshlakkad22@gmail.com', '123456bB', '7046167267', 'line 333, line 444', 'Near Ashok Vatika Society, L.H.Road', 'Surat', 'Gujarat', 'India', 'R', '395006', '2018-08-08', 'Male', '2018-08-20 16:12:13', '2018-08-20 16:12:13', 1, 1, '469819');
 
 -- --------------------------------------------------------
 
@@ -198,7 +181,7 @@ INSERT INTO `feedbacks` (`f_id`, `email`, `f_text`, `time`) VALUES
 CREATE TABLE `requests` (
   `r_id` int(10) NOT NULL,
   `acc_id` int(10) NOT NULL,
-  `card_loan` varchar(4) NOT NULL,
+  `card_req` varchar(5) DEFAULT NULL,
   `status_bit` varchar(5) NOT NULL,
   `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -207,10 +190,11 @@ CREATE TABLE `requests` (
 -- Dumping data for table `requests`
 --
 
-INSERT INTO `requests` (`r_id`, `acc_id`, `card_loan`, `status_bit`, `created_time`) VALUES
+INSERT INTO `requests` (`r_id`, `acc_id`, `card_req`, `status_bit`, `created_time`) VALUES
 (1, 4, 'card', '1', '2018-08-05 12:27:35'),
 (2, 19, 'card', '1', '2018-08-20 15:49:11'),
-(4, 20, 'card', '1', '2018-08-22 16:20:18');
+(4, 20, 'card', '1', '2018-08-22 16:20:18'),
+(5, 20, 'card', '0', '2018-10-18 21:15:40');
 
 -- --------------------------------------------------------
 
@@ -219,12 +203,46 @@ INSERT INTO `requests` (`r_id`, `acc_id`, `card_loan`, `status_bit`, `created_ti
 --
 
 CREATE TABLE `transactions` (
-  `t_id` int(10) NOT NULL,
-  `from_acc` int(10) NOT NULL,
-  `to_acc` int(10) NOT NULL,
-  `amount` int(10) NOT NULL,
-  `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `t_id` int(100) NOT NULL,
+  `from_acc` varchar(12) NOT NULL,
+  `to_acc` varchar(12) NOT NULL,
+  `amount` varchar(30) NOT NULL,
+  `created_time` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`t_id`, `from_acc`, `to_acc`, `amount`, `created_time`) VALUES
+(39, '007046167268', '007046167267', '79', '2018-10-16 02:29:55'),
+(42, '007046167267', '007046167268', '001', '2018-10-16 02:34:31'),
+(43, '007046167267', '007046167268', '001', '2018-10-16 02:35:35'),
+(44, '007046167267', '007046167268', '1', '2018-10-16 02:35:43'),
+(45, '007046167267', '007046167268', '1', '2018-10-16 02:37:09'),
+(46, '007046167267', '007046167268', '1', '2018-10-16 02:39:42'),
+(47, '007046167267', '007046167268', '1', '2018-10-16 02:40:17'),
+(48, '007046167267', '007046167268', '1', '2018-10-16 02:40:58'),
+(49, '007046167267', '007046167268', '1', '2018-10-16 02:40:59'),
+(50, '007046167267', '007046167268', '1', '2018-10-16 02:41:14'),
+(51, '007046167267', '007046167268', '11', '2018-10-16 02:41:46'),
+(52, '007046167267', '007046167268', '1', '2018-10-16 02:44:58'),
+(53, '007046167267', '007046167268', '1', '2018-10-16 02:46:50'),
+(54, '007046167267', '007046167268', '1', '2018-10-16 02:46:53'),
+(55, '007046167267', '007046167268', '1', '2018-10-16 02:48:20'),
+(56, '007046167267', '007046167268', '11', '2018-10-16 02:49:27'),
+(57, '007046167267', '007046167268', '1', '2018-10-16 02:53:34'),
+(58, '007046167267', '007046167268', '1', '2018-10-16 02:54:27'),
+(59, '007046167267', '007046167268', '1', '2018-10-16 02:55:14'),
+(60, '007046167267', '007046167268', '1', '2018-10-16 02:55:36'),
+(61, '007046167267', '007046167268', '1', '2018-10-16 03:04:12'),
+(62, '007046167267', '007046167268', '1', '2018-10-16 03:04:29'),
+(63, '007046167267', '007046167268', '1111', '2018-10-16 15:30:36'),
+(64, '007046167267', '007046167268', '112', '2018-10-17 16:31:25'),
+(65, '007046167267', '007046167268', '112', '2018-10-17 16:43:11'),
+(66, '007046167267', '007046167268', '112', '2018-10-17 18:22:31'),
+(67, '007046167267', '007046167268', '112', '2018-10-18 15:17:16'),
+(68, '007046167267', '007046167268', '112', '2018-10-18 20:04:43');
 
 --
 -- Indexes for dumped tables
@@ -235,14 +253,8 @@ CREATE TABLE `transactions` (
 --
 ALTER TABLE `accounts`
   ADD PRIMARY KEY (`acc_id`),
+  ADD UNIQUE KEY `acc_no` (`acc_no`),
   ADD KEY `c_id` (`c_id`);
-
---
--- Indexes for table `bank_atm`
---
-ALTER TABLE `bank_atm`
-  ADD PRIMARY KEY (`area`),
-  ADD UNIQUE KEY `bid` (`bid`);
 
 --
 -- Indexes for table `cards`
@@ -299,16 +311,10 @@ ALTER TABLE `accounts`
   MODIFY `acc_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
--- AUTO_INCREMENT for table `bank_atm`
---
-ALTER TABLE `bank_atm`
-  MODIFY `bid` int(10) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `cards`
 --
 ALTER TABLE `cards`
-  MODIFY `card_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `card_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `chats`
@@ -332,13 +338,13 @@ ALTER TABLE `feedbacks`
 -- AUTO_INCREMENT for table `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `r_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `r_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `t_id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `t_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 
 --
 -- Constraints for dumped tables
@@ -367,8 +373,8 @@ ALTER TABLE `requests`
 -- Constraints for table `transactions`
 --
 ALTER TABLE `transactions`
-  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`from_acc`) REFERENCES `accounts` (`acc_id`),
-  ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`to_acc`) REFERENCES `accounts` (`acc_id`);
+  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`from_acc`) REFERENCES `accounts` (`acc_no`),
+  ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`to_acc`) REFERENCES `accounts` (`acc_no`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
