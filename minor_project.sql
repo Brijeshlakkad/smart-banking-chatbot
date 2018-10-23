b55891428c6fe0600fd9f6e8e9678402f9695203
Brijeshlakkad <Brijeshlakkad@github.com>
2018-10-23 16:32:35 +0530

    Dropped chats table

--- minor_project.sql
+++ minor_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 18, 2018 at 04:24 PM
+-- Generation Time: Oct 23, 2018 at 11:01 AM
 -- Server version: 5.6.38
 -- PHP Version: 7.2.1
 
@@ -44,7 +44,7 @@ CREATE TABLE `accounts` (
 
 INSERT INTO `accounts` (`acc_id`, `c_id`, `acc_name`, `acc_type`, `balance`, `created_time`, `passcode`, `acc_no`, `status`) VALUES
 (4, 70, 'Brijesh Rameshbhai Lakkad', 'saving', '0', '2018-08-01 20:24:57', '123456', '007046167269', 1),
-(16, 1, 'Brijesh Rameshbhai Lakkad', 'saving', '2411', '2018-08-06 17:33:32', '-99', '007046167268', 1),
+(16, 1, 'Brijesh Rameshbhai Lakkad', 'saving', '2523', '2018-08-06 17:33:32', '-99', '007046167268', 1),
 (19, 100, 'Sanket Patel', 'saving', '0', '2018-08-20 15:44:59', '123456', '007046167290', 1),
 (20, 101, 'Brijesh Rameshbhai Lakkad', 'saving', '1889', '2018-08-20 16:15:47', '683249', '007046167267', 1);
 
@@ -73,40 +73,7 @@ CREATE TABLE `cards` (
 
 INSERT INTO `cards` (`card_id`, `c_id`, `acc_id`, `holder_name`, `till_month`, `till_year`, `csv`, `card_type`, `card_no`, `status`) VALUES
 (10, 70, 4, 'Brijesh Rameshbhai Lakkad', '08', '19', '494', 'Mastercard', '2685498962451745', 1),
-(38, 100, 19, 'Sanket Patel', '10', '19', '656', 'Mastercard', '5743344187443345', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `chats`
---
-
-CREATE TABLE `chats` (
-  `chat_id` int(10) NOT NULL,
-  `from_id` int(10) NOT NULL,
-  `to_id` int(10) NOT NULL,
-  `text` varchar(100) NOT NULL,
-  `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `chats`
---
-
-INSERT INTO `chats` (`chat_id`, `from_id`, `to_id`, `text`, `created_time`) VALUES
-(1, 14, 0, 'hey', '2018-07-28 21:11:04'),
-(4, 0, 19, 'Hello, i m Jon', '2018-07-28 21:39:33'),
-(8, 14, 0, 'Hey again', '2018-07-29 00:03:52'),
-(11, 14, 0, 'final', '2018-07-29 00:05:53'),
-(12, 19, 0, 'Hey', '2018-07-30 09:19:55'),
-(13, 19, 0, 'I want help', '2018-07-30 09:20:37'),
-(14, 0, 19, 'I m listening', '2018-07-30 09:21:02'),
-(15, 70, 0, 'hey', '2018-08-01 20:04:28'),
-(16, 70, 0, 'Hello', '2018-08-02 20:27:14'),
-(17, 101, 0, 'Hey, Jon', '2018-08-20 16:31:17'),
-(18, 0, 101, 'Hello, i m Jon. How can i help you?', '2018-07-28 21:39:33'),
-(19, 106, 0, 'Hi', '2018-08-23 00:50:48'),
-(20, 107, 0, 'hii', '2018-08-23 09:42:52');
+(55, 101, 20, 'Brijesh Rameshbhai Lakkad', '10', '19', '779', 'Mastercard', '8616058501975566', 0);
 
 -- --------------------------------------------------------
 
@@ -146,7 +113,7 @@ INSERT INTO `customers` (`cid`, `username`, `fname`, `lname`, `email`, `password
 (1, 'brijeshlakkad22222', 'Brijesh', 'Lakkad', 'lakkadbrijesh@gmail.com', '12345b', '7046167268', 'xyz', 'xyz', 'surat', 'gujarat', 'india', 'Rameshbhai', '395006', NULL, 'Male', '2018-07-27 08:56:55', '2018-07-27 16:33:37', 0, 1, '-99'),
 (70, 'Brijesh', 'Brijesh', 'Lakkad', 'brij79lakkad@gmail.com', '123456bB', '7046167269', '205, Nanddham Apartment', 'Near Ashok Vatika Society, L.H.Road', 'Surat', 'Gujarat', 'India', 'Rameshbhai', '395008', '2018-07-03', 'Male', '2018-07-31 13:44:07', '2018-07-31 13:44:07', 1, 1, '-99'),
 (100, 'sanketpatel2', 'Sanket', 'Patel', 'patelsanket864@gmail.com', '123456bB', '7046167290', '205, Nanddham Apartment', 'Near Ashok Vatika Society, L.H.Road', 'Surat', 'Gujarat', 'India', 'J', '395006', '2018-08-08', 'Male', '2018-08-20 15:13:11', '2018-08-20 15:13:11', 1, 0, '-99'),
-(101, 'brijeshlakkad22', 'Brijesh', 'Lakkad', 'brijeshlakkad22@gmail.com', '123456bB', '7046167267', 'line 333, line 444', 'Near Ashok Vatika Society, L.H.Road', 'Surat', 'Gujarat', 'India', 'R', '395006', '2018-08-08', 'Male', '2018-08-20 16:12:13', '2018-08-20 16:12:13', 1, 1, '469819');
+(101, 'Brij79', 'Brijesh', 'Lakkad', 'brijeshlakkad22@gmail.com', '123456bB', '7046167267', 'Line 2, Line 3', 'Near Ashok Vatika Society, L.H.Road', 'Surat', 'Gujarat', 'India', 'R', '395006', '2018-08-08', 'Male', '2018-08-20 16:12:13', '2018-08-20 16:12:13', 1, 1, '469819');
 
 -- --------------------------------------------------------
 
@@ -181,7 +148,7 @@ INSERT INTO `feedbacks` (`f_id`, `email`, `f_text`, `time`) VALUES
 CREATE TABLE `requests` (
   `r_id` int(10) NOT NULL,
   `acc_id` int(10) NOT NULL,
-  `card_req` varchar(5) DEFAULT NULL,
+  `card_loan` varchar(10) DEFAULT NULL,
   `status_bit` varchar(5) NOT NULL,
   `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -190,11 +157,10 @@ CREATE TABLE `requests` (
 -- Dumping data for table `requests`
 --
 
-INSERT INTO `requests` (`r_id`, `acc_id`, `card_req`, `status_bit`, `created_time`) VALUES
+INSERT INTO `requests` (`r_id`, `acc_id`, `card_loan`, `status_bit`, `created_time`) VALUES
 (1, 4, 'card', '1', '2018-08-05 12:27:35'),
 (2, 19, 'card', '1', '2018-08-20 15:49:11'),
-(4, 20, 'card', '1', '2018-08-22 16:20:18'),
-(5, 20, 'card', '0', '2018-10-18 21:15:40');
+(11, 20, 'card', '1', '2018-10-20 11:28:10');
 
 -- --------------------------------------------------------
 
@@ -242,7 +208,8 @@ INSERT INTO `transactions` (`t_id`, `from_acc`, `to_acc`, `amount`, `created_tim
 (65, '007046167267', '007046167268', '112', '2018-10-17 16:43:11'),
 (66, '007046167267', '007046167268', '112', '2018-10-17 18:22:31'),
 (67, '007046167267', '007046167268', '112', '2018-10-18 15:17:16'),
-(68, '007046167267', '007046167268', '112', '2018-10-18 20:04:43');
+(68, '007046167267', '007046167268', '112', '2018-10-18 20:04:43'),
+(69, '007046167267', '007046167268', '112', '2018-10-20 12:04:05');
 
 --
 -- Indexes for dumped tables
@@ -263,12 +230,6 @@ ALTER TABLE `cards`
   ADD PRIMARY KEY (`card_id`),
   ADD KEY `c_id` (`c_id`),
   ADD KEY `acc_id` (`acc_id`);
-
---
--- Indexes for table `chats`
---
-ALTER TABLE `chats`
-  ADD PRIMARY KEY (`chat_id`);
 
 --
 -- Indexes for table `customers`
@@ -314,13 +275,7 @@ ALTER TABLE `accounts`
 -- AUTO_INCREMENT for table `cards`
 --
 ALTER TABLE `cards`
-  MODIFY `card_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
-
---
--- AUTO_INCREMENT for table `chats`
---
-ALTER TABLE `chats`
-  MODIFY `chat_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `card_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT for table `customers`
@@ -338,13 +293,13 @@ ALTER TABLE `feedbacks`
 -- AUTO_INCREMENT for table `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `r_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `r_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `t_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+  MODIFY `t_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- Constraints for dumped tables
